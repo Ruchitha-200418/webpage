abe14d7e136aff34ffcbeae5cdbff61e497ef073
Ruchitha-200418 <ruchithapuli1808@gmail.com>
2025-08-15 09:55:12 +0530

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,3 @@
+let num1 = parseFloat(prompt("Enter dividend:"));
+let num2 = parseFloat(prompt("Enter divisor:"));
+alert("Quotient: " + (num1 / num2));
---
+++ index6.js
@@ -0,0 +1,3 @@
+let num1 = parseInt(prompt("Enter dividend:"));
+let num2 = parseInt(prompt("Enter divisor:"));
+alert("Remainder: " + (num1 % num2));
